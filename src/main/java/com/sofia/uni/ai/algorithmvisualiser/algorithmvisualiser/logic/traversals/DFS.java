55990d401033c4e23536844912b09b33bbd4568f
Bozhidar Dimitrov <dbozhidar@vmware.com>
2023-02-09 00:33:36 +0200

    DFS implementation

--- src/main/java/com/sofia/uni/ai/algorithmvisualiser/algorithmvisualiser/logic/traversals/DFS.java
+++ src/main/java/com/sofia/uni/ai/algorithmvisualiser/algorithmvisualiser/logic/traversals/DFS.java
@@ -1,17 +1,60 @@
 package com.sofia.uni.ai.algorithmvisualiser.algorithmvisualiser.logic.traversals;
 
-import com.sofia.uni.ai.algorithmvisualiser.algorithmvisualiser.logic.AbstractGraph;
-import com.sofia.uni.ai.algorithmvisualiser.algorithmvisualiser.logic.Edge;
-import com.sofia.uni.ai.algorithmvisualiser.algorithmvisualiser.logic.TraversalStepResult;
+import com.sofia.uni.ai.algorithmvisualiser.algorithmvisualiser.logic.*;
 
-import java.util.List;
+import java.util.*;
 
 public class DFS extends AbstractGraph {
+    private final LinkedList<TraversalStepResult> traversalStepResults;
+
     public DFS(List<Integer> nodes, List<Edge> edges) {
         super(nodes, edges);
+        this.traversalStepResults = new LinkedList<>();
+        this.traverse(1);
+        System.out.println("echo");
+    }
+
+    public void traverse(int start) {
+        Stack<Integer> stack = new Stack<>();
+        Set<Integer> visited = new HashSet<>();
+        stack.push(start);
+        visited.add(start);
+        this.traversalStepResults.add(new TraversalStepResult(
+                new State(start, NodeColor.RED), copyStack(stack)
+        ));
+        while (!stack.isEmpty()) {
+            int current = stack.pop();
+
+            for (Map<Integer, LinkedList<Integer>> m : graph) {
+                if (m.containsKey(current)) {
+                    LinkedList<Integer> neighbors = m.get(current);
+                    for (int neighbor : neighbors) {
+                        if (!visited.contains(neighbor)) {
+                            stack.push(neighbor);
+                            visited.add(neighbor);
+                            this.traversalStepResults.add(new TraversalStepResult(
+                                    new State(neighbor, NodeColor.RED), copyStack(stack)
+                            ));
+                        }
+                    }
+                }
+            }
+            this.traversalStepResults.add(new TraversalStepResult(
+                    new State(current, NodeColor.GREEN), copyStack(stack)
+            ));
+        }
+    }
+
+    public LinkedList<TraversalStepResult> getTraversalStepResults() {
+        return traversalStepResults;
     }
 
     public TraversalStepResult getNextState(int index) {
         return null;
+    }
+
+    private Deque<Integer> copyStack(Stack stack) {
+
+        return new LinkedList<>(stack);
     }
 }
