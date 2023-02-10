bcf0a38e4376e5aa26e727a26ac879cf5b77f340
Bozhidar Dimitrov <dbozhidar@vmware.com>
2023-02-10 13:27:04 +0200

    Mountain graph

--- src/main/java/com/sofia/uni/ai/algorithmvisualiser/algorithmvisualiser/drawing/TextLayoutDetails.java
+++ src/main/java/com/sofia/uni/ai/algorithmvisualiser/algorithmvisualiser/drawing/TextLayoutDetails.java
@@ -1,4 +1,4 @@
 package com.sofia.uni.ai.algorithmvisualiser.algorithmvisualiser.drawing;
 
-public record TextLayoutDetails(double x, double y, String text) {
+public record TextLayoutDetails(double x, double y, String text, Double wrappingWidth, int fontSize) {
 }
